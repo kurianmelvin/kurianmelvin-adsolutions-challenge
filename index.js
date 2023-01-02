2a7d8aa4563b218f39af020dd4e6807d72cd112b
Melvin <kurianmelvin@users.noreply.github.com>
2023-01-02 05:04:42 +0000

    Update index.js

--- index.js
+++ index.js
@@ -10,4 +10,4 @@ http.createServer(function (req, res) {
 }).listen(42780);
 
 const myAddr = 'http://localhost:42780/';
-open('http://preview.adsolutions.com/?url=ad01&placements[]=leader1&placementIds[leader1][tag]='+myAddr);//must be https in chrome; https://developer.chrome.com/blog/private-network-access-update/ | https://developer.chrome.com/blog/private-network-access-preflight/
+open('https://preview.adsolutions.com/?url=ad01&placements[]=leader1&placementIds[leader1][tag]='+myAddr);//must be https in chrome; https://developer.chrome.com/blog/private-network-access-update/ | https://developer.chrome.com/blog/private-network-access-preflight/
